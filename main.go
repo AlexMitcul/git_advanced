247816e9136e1f12154531ab588fa6538d249a82
AlexandruMitcul <alexandru.mitcul@outlook.com>
2024-05-28 18:47:18 +0100

    go linear seach inversed

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ func linearSearchRecursive(arr []int, target, index int) int {
 	if index >= len(arr) {
 		return -1 // Base case: target not found in the array
 	}
-	if arr[index] == target {
+	if target == arr[index] {
 		return index // Base case: target found
 	}
 	return linearSearchRecursive(arr, target, index+1) // Recursive call
